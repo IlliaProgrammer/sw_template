ad88d95b23073e0f575c288203f68fe60c388c40
Illia Fialkivskiji <fialkivskiji@gmail.com>
2023-04-23 19:57:07 +0300

    Updates

---
+++ precache-manifest.f0bddb56685802ab5bb3b48f7aff2b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2abd7241006658ab5ce",
+    "url": "/sw_template/static/css/main.28b4cd7b.chunk.css"
+  },
+  {
+    "revision": "c2abd7241006658ab5ce",
+    "url": "/sw_template/static/js/main.c2abd724.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/sw_template/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/sw_template/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "b5aba173fa1818bd0293",
+    "url": "/sw_template/static/js/runtime~main.b5aba173.js"
+  },
+  {
+    "revision": "74f83e1e96b6d28443a04eb96fc2129f",
+    "url": "/sw_template/index.html"
+  }
+];
